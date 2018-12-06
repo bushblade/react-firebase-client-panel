45800c95a4ecbc6e12ea5a4a1e4d8acaae186e98
Will Adams <bushblade@gmail.com>
2018-12-06 21:47:50 +0000

    better

--- src/components/layouts/AppNavbar.js
+++ src/components/layouts/AppNavbar.js
@@ -2,22 +2,27 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
 class AppNavbar extends Component {
+  state = {
+    open: false
+  }
+  toggleOpen = () => this.setState(({ open }) => ({ open: !open }))
+
   render() {
+    const {
+      state: { open },
+      toggleOpen
+    } = this
+
     return (
       <nav className="navbar navbar-expand-md navbar-dark bg-primary mb-4">
         <div className="container">
           <Link to="/" className="navbar-brand">
             Client Panel
           </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            onClick={() => {
-              document.getElementById('navbarMain').classList.toggle('collapse')
-            }}>
+          <button className="navbar-toggler" type="button" onClick={toggleOpen}>
             <span className="navbar-toggler-icon" />
           </button>
-          <div className="collapse navbar-collapse" id="navbarMain">
+          <div className={`navbar-collapse${!open ? ' collapse' : ''}`} id="navbarMain">
             <ul className="navbar-nav mr-auto">
               <li className="nav-item">
                 <Link to="/" className="nav-link">
